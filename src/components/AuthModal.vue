11d3a821b05ff9f910b14364bf3f7c38ce69d80e
Shivani vyas <2020bcs140@sggs.ac.in>
2024-04-01 12:27:42 +0530

    first commit

---
+++ src/components/AuthModal.vue
@@ -0,0 +1,42 @@
+<script  setup>
+import { ref,defineProps } from 'vue';
+const open = ref(false);
+
+const props= defineProps((['isLogin']))
+const showModal = () => {
+  open.value = true;
+};
+
+const handleOk = (e) => {
+ 
+  open.value = false;
+};
+const title= props.isLogin ?'Login' :'Signup'
+
+</script>
+
+
+<template>
+  <div>
+    <a-button type="primary" @click="showModal" class="btn">{{ title}}</a-button>
+    <a-modal v-model:open="open" title="title" @ok="handleOk">
+      <a-input  class="input" v-if="!isLogin" v-model:value="value" placeholder="Username" />
+      <a-input class="input" v-model:value="value" placeholder="Email" />
+      <a-input  class="input" v-model:value="value" placeholder="Password" />
+
+      <!-- <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p> -->
+    </a-modal>
+  </div>
+</template>
+
+<style scoped>
+ .btn {
+margin-left:10px;
+
+ }
+ .input{
+  margin-top:5px;
+ }
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,62 @@
+<script setup>
+import {RouterLink} from "vue-router"
+import container from "./container.vue";
+import AuthModal from "./AuthModal.vue"
+import {ref} from "vue"
+
+const value=ref("")
+const isAuthenticated=ref(false)
+const onSearch=()=>{}
+</script>
+<template>
+   <ALayoutHeader>
+    <container>
+        <div class="nav-container">
+            <div class="right-content">
+    <RouterLink to="/">Instagram</RouterLink>
+    
+        <AInputSearch
+        v-model:value="value"
+        placeholder="username..."
+        style="width: 200px"
+        @search="onSearch"
+        />
+    </div>
+    <div class="left-content" v-if="!isAuthenticated">
+    <!-- <AButton type="primary">sign up</AButton> -->
+    <!-- <AButton type="primary"> LogIn</AButton> -->
+    <AuthModal :isLogin="false"/>
+    <AuthModal :isLogin="true"/>
+</div>
+<div class="left-content" v-else>
+    <a-button type="primary">Profile</a-button>
+    <a-button type="primary">Logout</a-button>
+</div>
+
+</div>
+</container>
+   </ALayoutHeader>
+</template>
+<style scoped>
+.nav-container{
+    background-color: aqua;
+    display: flex;
+    justify-content: space-between;
+}
+.right-content{
+    display: flex;
+    align-items:center ;
+}
+.right-content a{
+    margin-right:10px;
+
+}
+.left-content{
+    display: flex;
+    align-items: center;
+}
+.left-content button{
+    margin-left:10px;
+    
+}
+</style>
