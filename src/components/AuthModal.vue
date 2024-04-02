6c7043a3a92515fe23f35f1d8fb12574a8b9aed5
Sunil Subramanya <subramanya.sunil@gmail.com>
2024-04-02 10:51:38 +0530

    fix antd styling and modal visible state

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -19,7 +19,7 @@ const title= props.isLogin ?'Login' :'Signup'
 <template>
   <div>
     <a-button type="primary" @click="showModal" class="btn">{{ title}}</a-button>
-    <a-modal v-model:open="open" title="title" @ok="handleOk">
+    <a-modal v-model:visible="open" title="title" @ok="handleOk">
       <a-input  class="input" v-if="!isLogin" v-model:value="value" placeholder="Username" />
       <a-input class="input" v-model:value="value" placeholder="Email" />
       <a-input  class="input" v-model:value="value" placeholder="Password" />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
+
 <script setup>
+import "ant-design-vue/dist/antd.css";
 
 </script>
 
